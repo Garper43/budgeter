4c1f6e1318f38af813a4990a639f11288bb57205
Garper43 <65208305+Garper43@users.noreply.github.com>
2020-11-24 22:08:25 -0800

    should work

--- budgeter.java
+++ budgeter.java
@@ -14,120 +14,72 @@ public class budgeter {
     final static int MONTH = 31; //# of days in a month
     public static void main(String arg[]) {
         Scanner scan = new Scanner(System.in);
-        Double[] income , expense;
+        Double income , expense;
         int expenseType;   
-        income = get_income_categories( scan );    
-        income = get_income( scan , income ); 
+        int incomeCategories = get_income_categories( scan );    
+        income = get_income( scan , incomeCategories ); 
         expenseType = get_expense_type( scan );
-        expense = get_expense_categories( scan );
-        expense = get_expense( scan , expense );
+        int expenseCategories = get_expense_categories( scan );
+        expense = get_expense( scan , expenseCategories );
         calculate( income , expense , expenseType );
         scan.close();
     }
 
-    public static Double[] get_income_categories( Scanner scan ) { //returns an empty Double array
-        boolean complete = false;
-        int incomeCategories = 0;
+    public static int get_income_categories( Scanner scan ) { //returns an empty Double array
         System.out.println("This program asks for your monthly income and expenses, then tells you your net monthly income. ");
         System.out.println("How many categories of income? ");
         System.out.println("");
-        while( !complete ) {
-            try {
-                incomeCategories = Integer.parseInt(scan.nextLine());
-                complete = true;
-            } catch( Exception e ) {
-                System.out.println("Please try again ");
-                scan.nextLine();
-            }
-        }
-        return new Double[incomeCategories]; 
+        int incomeCategories = Integer.parseInt(scan.nextLine());
+        return incomeCategories; 
     } 
 
-    public static Double[] get_income( Scanner scan , Double[] income ) { //returns an income array 
-        System.out.println("");                                         //( I used arrays because they'd be useful if I needed to add functionality and because i felt like it )
-        for( int i = 0 ; i < income.length ; i++ ) {
-            try { //takes income input
-                System.out.println("Next income amount? ");    
-                Double input = Double.parseDouble( scan.nextLine() ); 
-                income[ i ] = input;  
-            } catch( Exception e ) {
-                System.out.println("Please try again ");
-                scan.nextLine();
-                i -= 1; //prevents waste of loops on failed attmets
-            }     
+    public static Double get_income( Scanner scan , int incomeCategories ) { //returns an income array  
+        Double income = 0.0;                                  
+        for( int i = 0 ; i < incomeCategories ; i++ ) {
+            System.out.println("Next income amount? ");    
+            Double input = Double.parseDouble( scan.nextLine() ); 
+            income += input;     
         }
         return income;
     }
 
     public static int get_expense_type( Scanner scan ) { //gets expense type that will later be use by calculate()
-        int expenseType = 0;
-        boolean complete = false;
         System.out.println("");
-        while( !complete ) {
-            try {
-                System.out.println("Enter 1) monthly or 2) daily expenses? ");
-                expenseType = Integer.parseInt(scan.nextLine());
-                if( expenseType != 1 && expenseType != 2 ) { throw new Exception("invalid expenseType"); }
-                complete = true;
-            } catch( Exception e ) {
-                System.out.println("Please try again ");
-                scan.nextLine();
-            }
-        }
-        return expenseType;
+        System.out.println("Enter 1) monthly or 2) daily expenses? ");
+        return Integer.parseInt(scan.nextLine());
     }
 
-    public static Double[] get_expense_categories( Scanner scan ) { //returns an empty Double array
-        boolean complete = false;
-        int expenseCategories = 0;
-        System.out.println("");
-        while( !complete ) {
-            try {
-                System.out.println("How many categories of expense? ");
-                expenseCategories = Integer.parseInt(scan.nextLine());
-                complete = true;
-            } catch( Exception e ) {
-                System.out.println("Please try again ");
-                scan.nextLine();
-            }
-        }
-        return new Double[expenseCategories];
+    public static int get_expense_categories( Scanner scan ) { //returns an empty Double array
+        System.out.println("How many categories of expense? ");
+        int expenseCategories = Integer.parseInt(scan.nextLine());
+        return expenseCategories;
     }
 
-    public static Double[] get_expense( Scanner scan , Double[] expense ) { //returns an expense array
+    public static Double get_expense( Scanner scan , int expenseCategories ) { //returns an expense array
+        Double expense = 0.0;
         Double input;
-        System.out.println("");
-        for( int i = 0 ; i < expense.length ; i++ ) {
-            try {
-                System.out.println("Next expense amount? ");    
-                input = Double.parseDouble( scan.nextLine() ); 
-                expense[ i ] = input;  
-            } catch( Exception e ) {
-                scan.nextLine();
-                i -= 1; //prevents waste of loops on failed attmets
-            }     
+        for( int i = 0 ; i < expenseCategories ; i++ ) {
+            System.out.println("Next expense amount? ");    
+            input = Double.parseDouble( scan.nextLine() ); 
+            expense += input;      
         }
         return expense;
     }
 
-    public static void calculate( Double[] income , Double[] expense , int expenseType ) {
-        Double totalIncome = 0.0;
-        Double totalExpense = 0.0;
+    public static void calculate( Double income , Double expense , int expenseType ) {
         Double total = 0.0;
         String string;
         System.out.println("");
-        //add numbers from the arrays
-        for( int i = 0 ; i < income.length ; i++ ) { totalIncome += income[i]; }
-        for( int i = 0 ; i < expense.length ; i++ ) { totalExpense += expense[i]; }
         //round numbers
-        if( expenseType == 2 ) { totalExpense = totalExpense * MONTH; }
-        totalIncome = Math.round(totalIncome * 100.0) / 100.0;
-        totalExpense = Math.round(totalExpense * 100.0) / 100.0;
+        if( expenseType == 2 ) { expense = expense * MONTH; }
+        income = Math.round(income * 100.0) / 100.0;
+        expense = Math.round(expense * 100.0) / 100.0;
         //find the total earnings/spendings
-        total = totalIncome - totalExpense;
+        total = income - expense;
+        total = Math.round(total * 100.0) / 100.0;;
         //output
-        System.out.println("Total income = $"+totalIncome+" ($"+Math.round(totalIncome/MONTH * 100.0) / 100.0+"/day)");
-        System.out.println("Total expense = $"+totalExpense+" ($"+Math.round(totalExpense/MONTH * 100.0) / 100.0+"/day)");
+        System.out.println("Total income = $"+income+" ($"+Math.round(income/MONTH * 100.0) / 100.0+"/day)");
+        System.out.println("Total expense = $"+expense+" ($"+Math.round(expense/MONTH * 100.0) / 100.0+"/day)");
         System.out.println("");
         if( total > 250f )                       { string = "Big Saver"; 
         } else if( total <= 250.0 && total > 0 )  {  string = "Saver";
